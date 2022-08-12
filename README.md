698375f13a520912bd344641b5855b2ef53f5f8a
Gabriel Pleimann <gdpleima02@gmail.com>
2022-08-12 11:57:23 -0500

    Finished the base of the etch-a-sketch

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Odin-EtchASketch
+
+Now that the RPS UI has been made, it is time to move on to the next project. This project is to build a mix between a notepad and an EtchASketch. This will be to make a grid of squares and allow the user to alter the size of this grid. I will also need to create hover effects when a user's mouse goes over a square to create a EtchASketch style of look.
---
+++ javascript.js
@@ -0,0 +1,42 @@
+const container = document.querySelector(".container");
+
+let width = 16;
+createSquares();
+
+const widthButton = document.querySelector('.button');
+widthButton.addEventListener('click', () =>{
+    do{
+        width = prompt("How many squares wide would you like to make the box?");
+    } while (width <= 0 || width > 100)
+    removeSquares();
+    createSquares();
+});
+
+function removeSquares(){
+    const removal2 = document.querySelectorAll(".down");
+    removal2.forEach(element2 => {
+        element2.remove();
+    });
+    const removal = document.querySelectorAll(".across");
+    removal.forEach(element => {
+        element.remove();
+    });
+}
+function createSquares(){
+    for(let i = 0; i < width; i++){
+        const div = document.createElement('div');
+        for (let j = 0; j < width; j++){
+            const div2 = document.createElement('div');
+            div2.classList.add('down');
+            div.appendChild(div2);
+        }
+        div.classList.add('across');
+        container.appendChild(div);
+    }
+    const boxes = document.getElementsByClassName("down");
+    for (let i = 0; i < boxes.length; i++){
+    boxes[i].addEventListener("mouseover", () => {
+        boxes[i].classList.add('hover');
+    });
+}
+}
